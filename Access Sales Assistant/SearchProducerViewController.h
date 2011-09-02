53deab5d530f3a32a8b23dc29b7ce08fecb1bcde
Ross Chapman <ross.chapman@me.com>
2011-09-02 11:50:48 -0400

    Added Search producer view controller classes

---
+++ Access Sales Assistant/SearchProducerViewController.h
@@ -0,0 +1,17 @@
+//
+//  SearchProducerViewController.h
+//  Access Sales Assistant
+//
+//  Created by Easwara Reddy Illuri on 9/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BaseDetailViewController.h"
+
+@interface SearchProducerViewController : BaseDetailViewController
+
+
+@property(nonatomic,strong) IBOutlet UIToolbar* toolBar;
+@end
---
+++ Access Sales Assistant/SearchProducerViewController.m
@@ -0,0 +1,54 @@
+//
+//  SearchProducerViewController.m
+//  Access Sales Assistant
+//
+//  Created by Easwara Reddy Illuri on 9/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchProducerViewController.h"
+
+@implementation SearchProducerViewController
+@synthesize toolBar = _toolBar;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    self.baseToolbar = _toolBar;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	return YES;
+}
+
+@end
